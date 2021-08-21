9b5b9193afccf183b45853c217382a2d4a5946fc
Patrick Kimanthi <kimanthip@gmail.com>
2021-08-21 19:52:44 +0300

    refinement on auth routes

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,10 @@
-from flask import render_template, redirect, url_for,flash,request
+from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required
 from . import auth
 from models.user import User
 from .forms import LoginForm, RegistrationForm
 from .. import db
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -14,14 +15,14 @@ def login():
 
     if login_form.validate_on_submit():
         user = User.query.filter_by(email=login_form.email.data).first()
-        
+
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user, login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.home', cid=0))
-        
+            return redirect(request.args.get('next') or url_for('main.home'))
+
         flash('Invalid username or password')
-    
-    title = 'Pitches - login'
+
+    title = 'ZetuFeed - login'
     return render_template('auth/login.html', login_form=login_form, title=title)
 
 
@@ -33,14 +34,15 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(first_name=form.first_name.data, other_names=form.other_names.data, email=form.email.data,
-                    username=form.username.data, password=form.password.data, profile_pic_path="photos/default_user_pic.png")
+                    username=form.username.data, password=form.password.data, profile_pic_path="photos/users/default_user_pic.png")
         db.session.add(user)
         db.session.commit()
 
         return redirect(url_for('auth.login'))
-        title = "Pitches - new account"
-        
-    return render_template('auth/register.html', registration_form=form)
+    title = "ZetuFeed - new account"
+
+    return render_template('auth/register.html', registration_form=form, title=title)
+
 
 @auth.route('/logout')
 @login_required
@@ -49,4 +51,4 @@ def logout():
     Function that logs out the user from the application
     '''
     logout_user()
-    return redirect(url_for("main.index"))
+    return redirect(url_for("main.home"))
