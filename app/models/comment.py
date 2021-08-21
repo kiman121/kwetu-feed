c9097124df19d305e5240d4f00ddc555643b84f5
Patrick Kimanthi <kimanthip@gmail.com>
2021-08-21 04:14:21 +0300

    add comment model

---
+++ app/models/comment.py
@@ -0,0 +1,14 @@
+from . import db
+from datetime import datetime
+
+class Comment(db.Model):
+    '''
+    Class that creates comment objects
+    '''
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
