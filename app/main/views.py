93da3034a9e6053ca541ae3a40f996f835bd070b
Patrick Kimanthi <kimanthip@gmail.com>
2021-08-21 19:54:32 +0300

    extra properties for the home route

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,10 @@
 from flask import render_template, request, url_for, abort
 from . import main
+from .forms import LoginForm, RegistrationForm
+from models.category import Category
+from models.comment import Comment
+from models.post import Post
+from models.user import User
 
 
 @main.route('/')
@@ -7,4 +12,7 @@ def home():
     '''
     View function that renders the index page
     '''
-    return render_template('index.html')
+    title = "ZetuFeed - home"
+    categories = Category.get_categories()
+
+    return render_template('index.html', title=title, categories=categories)
