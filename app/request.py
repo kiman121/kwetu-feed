d680c0178acbd36acfff4e5add37e906ed257cd8
Patrick Kimanthi <kimanthip@gmail.com>
2021-08-22 05:44:53 +0300

    add a get quote (random) request

---
+++ app/request.py
@@ -0,0 +1,12 @@
+import urllib.request,json
+from models.quote import Quote
+
+
+def get_quote():
+    quote_url = 'http://quotes.stormconsultancy.co.uk/random.json'
+
+    with urllib.request.urlopen(quote_url) as url:
+        quote_data = url.read()
+        quote_response = json.loads(quote_data)
+
+    return quote_response
