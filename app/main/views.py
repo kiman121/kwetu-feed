0f8f8b2051540d9a7e9cedbb019925cc96d264ec
Patrick Kimanthi <kimanthip@gmail.com>
2021-08-22 13:37:09 +0300

    grouping view variables to a data dictionary

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,14 @@
-from flask import render_template, request, url_for, abort
+from flask import render_template,redirect, request, url_for, abort
 from flask_login import login_required, current_user
 from . import main
 from ..import db, photos
 from ..request import get_quote
-from .forms import PostForm, AddComment
+from .forms import PostForm, AddCommentForm, DeletePostForm, EditPostForm
 from models.category import Category
 from models.comment import Comment
 from models.post import Post
 from models.user import User
+from datetime import datetime
 
 
 @main.route('/')
@@ -15,12 +16,18 @@ def home():
     '''
     View function that renders the index page
     '''
-    title = "ZetuFeed - home"
-    new_post_form = PostForm()
-    categories = Category.get_categories()
-    random_quote = get_quote()
+    data = {
+        "title": "ZetuFeed - home",
+        "categories": Category.get_categories(),
+        "new_post_form": PostForm(),
+        "random_quote": get_quote(),
+        "featured_post": Post.featured_post(),
+        "posts": Post.get_posts(),
+        "delete_form": DeletePostForm(),
+        "edit_post_form": EditPostForm()
+    }
 
-    return render_template('index.html', title=title, categories=categories, new_post_form=new_post_form, random_quote=random_quote)
+    return render_template('index.html', context=data)
 
 @main.route('/post/new', methods=['GET', 'POST'])
 @login_required
@@ -30,7 +37,7 @@ def new_post():
     '''
     form = PostForm()
     request_endpoint = form.endpoint.data
-    print(request_endpoint)
+
     if request_endpoint == 'main.profile':
         redirect_to = url_for('.profile', userid=current_user.id)
     elif request_endpoint == 'main.home':
@@ -46,4 +53,45 @@ def new_post():
         db.session.add(new_post)
         db.session.commit()
 
+    return redirect(redirect_to)
+
+
+@main.route('/post/delete', methods=['Get','POST'])
+@login_required
+def delete_post():
+    '''
+    View unction that handles delete post request
+    '''
+    form = DeletePost()
+    post = Post.query.filter_by(id=form.post_id.data).first()
+    db.session.delete(post)
+    db.session.commit()
+    
+    return redirect(url_for('.home'))
+
+@main.route('/post/edit', methods=['Get','POST'])
+# @login_required
+def edit_post():
+    '''
+    View function that handles edit post request
+    '''
+    form = EditPostForm()
+    post = Post.query.filter_by(id=form.post_id.data).first()
+    
+    request_endpoint = form.endpoint.data
+
+    if request_endpoint == 'main.profile':
+        redirect_to = url_for('.profile', userid=current_user.id)
+    elif request_endpoint == 'main.home':
+        redirect_to = url_for('.home')
+
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.post = form.post.data
+        post.updated_at = datetime.utcnow()
+        post.category_id = form.category.data.id
+
+        db.session.add(post)
+        db.session.commit()
+        
     return redirect(redirect_to)
