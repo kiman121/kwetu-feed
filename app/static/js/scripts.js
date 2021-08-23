a4414109b17fa29382b53bea5f481b0b32818b7b
Patrick Kimanthi <kimanthip@gmail.com>
2021-08-23 07:36:57 +0300

    add code to open add and delete comments modals

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     $(document).on("click", ".delete-post", function(e){
         e.preventDefault()
 
-        var postId = $(this).data('postid')
+        var postId = $(this).parents(".blog-post").data('postid')
         $(".selected-post-delete").val(postId)
         $("#delete_post").modal("show");
     });
@@ -19,19 +19,37 @@ $(document).ready(function () {
     $(document).on("click", ".edit-post", function(e){
         e.preventDefault()
         // Get post values
-        var postId = $(this).data('postid'),
+        var postId = $(this).parents(".blog-post").data('postid'),
             categorId = parseInt($(this).parents('.blog-post').find('.category-id').val()),
             postTitle = $(this).parents('.blog-post').find('.post-title').val(),
             postDetail = $(this).parents('.blog-post').find('.post-detail').val();
-
         // Set edit form values
         $("form.form-edit-post #category").val(categorId).change();
         $("form.form-edit-post #title").val(postTitle);
         $("form.form-edit-post #post").val(postDetail);
         $("form.form-edit-post .selected-post-edit").val(postId);
-
         // Open modal
         $("#edit_post").modal("show");
+    });
 
+    $(document).on("click", ".add-comment", function(e){
+        e.preventDefault()
+
+        var postId = $(this).parents(".blog-post").data('postid')
+            userAuthStatus = $(this).data('userstatus');
+        if (userAuthStatus === "not authenticated"){
+            $("#user_prompt").modal("show");
+        } else {
+            $(".selected-post-comment").val(postId)
+            $("#add_comment").modal("show");  
+        }    
+    });
+
+    $(document).on("click", ".delete-comment", function(e){
+        e.preventDefault()
+
+        var commentId = $(this).parents(".comments-wrapper").data('commentid');
+            $(".selected-comment-delete").val(commentId)
+            $("#delete_comment").modal("show"); 
     });
 });
