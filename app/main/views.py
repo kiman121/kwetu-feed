43ce2bea46d21e46dbf15ea3327ef508513b6dc1
Patrick Kimanthi <kimanthip@gmail.com>
2021-08-23 07:34:34 +0300

    add profile, delete comment and add_comment view functions

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,15 @@
-from flask import render_template,redirect, request, url_for, abort
+from flask import render_template, redirect, request, url_for, abort
 from flask_login import login_required, current_user
 from . import main
 from ..import db, photos
-from ..request import get_quote
-from .forms import PostForm, AddCommentForm, DeletePostForm, EditPostForm
+from ..request import get_quote, post_comment_count
+from .forms import PostForm, AddCommentForm, DeletePostForm, EditPostForm, DeleteCommentForm
 from models.category import Category
 from models.comment import Comment
 from models.post import Post
 from models.user import User
 from datetime import datetime
+from sqlalchemy import func
 
 
 @main.route('/')
@@ -24,10 +25,15 @@ def home():
         "featured_post": Post.featured_post(),
         "posts": Post.get_posts(),
         "delete_form": DeletePostForm(),
-        "edit_post_form": EditPostForm()
+        "edit_post_form": EditPostForm(),
+        "add_comment": AddCommentForm(),
+        "post_comments": Comment.get_comments(),
+        "post_comment_count": post_comment_count(),
+        "delete_comment_post": DeleteCommentForm()
     }
 
     return render_template('index.html', context=data)
+
 
 @main.route('/post/new', methods=['GET', 'POST'])
 @login_required
@@ -56,28 +62,30 @@ def new_post():
     return redirect(redirect_to)
 
 
-@main.route('/post/delete', methods=['Get','POST'])
+@main.route('/post/delete', methods=['Get', 'POST'])
 @login_required
 def delete_post():
     '''
-    View unction that handles delete post request
+    View function that handles delete post request
     '''
-    form = DeletePost()
+    form = DeletePostForm()
     post = Post.query.filter_by(id=form.post_id.data).first()
+    Comment.query.filter(Comment.post_id == form.post_id.data).delete()
     db.session.delete(post)
     db.session.commit()
-    
+
     return redirect(url_for('.home'))
 
-@main.route('/post/edit', methods=['Get','POST'])
-# @login_required
+
+@main.route('/post/edit', methods=['Get', 'POST'])
+@login_required
 def edit_post():
     '''
     View function that handles edit post request
     '''
     form = EditPostForm()
     post = Post.query.filter_by(id=form.post_id.data).first()
-    
+
     request_endpoint = form.endpoint.data
 
     if request_endpoint == 'main.profile':
@@ -93,5 +101,58 @@ def edit_post():
 
         db.session.add(post)
         db.session.commit()
-        
+
     return redirect(redirect_to)
+
+
+@main.route('/post/comment', methods=['GET', 'POST'])
+@login_required
+def add_comment():
+    '''
+    View function that handles add comment request
+    '''
+    form = AddCommentForm()
+
+    request_endpoint = form.endpoint.data
+
+    if request_endpoint == 'main.profile':
+        redirect_to = url_for('.profile', userid=current_user.id)
+    elif request_endpoint == 'main.home':
+        redirect_to = url_for('.home')
+
+    if form.validate_on_submit():
+
+        new_comment = Comment(post_id=form.post_id.data, user_id=current_user.get_id(
+        ), comments=form.comment.data)
+
+        db.session.add(new_comment)
+        db.session.commit()
+
+    return redirect(redirect_to)
+
+
+@main.route('/post/comment/delete', methods=['GET', 'POST'])
+@login_required
+def delete_comment():
+    '''
+    View function that handles delete comment
+    '''
+    form = DeleteCommentForm()
+    comment = Comment.query.filter_by(id=form.comment_id.data).first()
+    db.session.delete(comment)
+    db.session.commit()
+
+    return redirect(url_for('.home'))
+
+@main.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    '''
+    View function that renders a user profile
+    '''
+    data = {
+        "title": "ZetuFeed - home",
+        "categories": Category.get_categories()
+    }
+    
+    return render_template('profile.html', context = data)
