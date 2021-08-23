e345fe9eaa1b2779087d8285af95b361ff1476fa
Patrick Kimanthi <kimanthip@gmail.com>
2021-08-23 07:31:18 +0300

    add post_comment_count function

--- app/request.py
+++ app/request.py
@@ -1,8 +1,12 @@
 import urllib.request,json
 from models.quote import Quote
-
+from models.post import Post
+from models.comment import Comment
 
 def get_quote():
+    '''
+    Function that retrieves quotes
+    '''
     quote_url = 'http://quotes.stormconsultancy.co.uk/random.json'
 
     with urllib.request.urlopen(quote_url) as url:
@@ -10,3 +14,26 @@ def get_quote():
         quote_response = json.loads(quote_data)
 
     return quote_response
+
+def post_comment_count():
+    '''
+    Function that gets post comments
+    '''
+    posts = Post.get_posts()
+    comments = Comment.get_comments()
+
+    post_comments = []
+
+    if posts:
+        for post in posts:
+            post_id = post.id
+            count = 0
+            if comments:
+                for comment in comments:
+                    if comment.post_id == post_id:
+                        count +=1
+            
+            post_comments.append({"post_id":post.id, "num_comments":count})
+            count = 0
+            
+    return post_comments
