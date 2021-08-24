e7edb12118235e7b2beb92ae09070eed042772d5
Patrick Kimanthi <kimanthip@gmail.com>
2021-08-24 08:10:42 +0300

    heroku deployment run

--- manage.py
+++ manage.py
@@ -28,10 +28,12 @@ app.jinja_env.filters['datetimeformat'] = datetimeformat
 def test():
     '''
     Run the unit tests.
+    
     '''
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 @manager.shell
 def make_shell_context():
