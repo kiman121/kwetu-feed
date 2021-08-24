f41878e4d7140be841543f18121b6571d44508f8
Patrick Kimanthi <kimanthip@gmail.com>
2021-08-24 10:42:49 +0300

    fetch secret key from environment

--- configs/base_config.py
+++ configs/base_config.py
@@ -2,7 +2,8 @@ import secrets
 import os
 class Config:
     # FLASK_APP = os.environ('FLASK_APP')
-    SECRET_KEY = secrets.token_hex(16)
+    # SECRET_KEY = secrets.token_hex(16)
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos/users'
 
@@ -30,7 +31,7 @@ class DevConfig(Config):
     POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
    
-   
+
 config_options = {
     'development':DevConfig,
     'production':ProdConfig,
